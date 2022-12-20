5141b3ab735f6128d2570edce6bff66ffed82f65
RinaRiwayani <120904891+RinaRiwayani@users.noreply.github.com>
2022-12-20 11:31:26 +0700

    Add files via upload

---
+++ KategoriBarangModel.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uapp;
+
+/**
+ *
+ * @author A S U S
+ */
+class KategoriBarangModel {
+
+    void updKategori(KategoriBarang ktgb) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void delKategori(KategoriBarang ktgb) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void addKategori(KategoriBarang ktgb) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ Uapp.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package uapp;
+
+import db.BarangModel;
+import db.DBHelper;
+import db.MakananModel;
+import db.PenjualanModel;
+
+/**
+ *
+ * @author A S U S
+ */
+public class Uapp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        DBHelper.getConnection();
+        BarangModel bm = new BarangModel();
+        //nambahin kodingan
+//        brg.updBarang(brg);
+//        bm.addBarang(bm);
+        //hapus barang
+//        bm.deleteBarang(brg);
+//                
+//namnah makananan
+//        MakananModel mm = new MakananModel();
+//       Makanan mkn = new Makanan("2", "nama makanan",1000,1,0,3);
+//        //nambahin makanana
+//        mkn.updMakanan(mkn);
+//        mkn.addMakanan(mkn);
+//        //hapus makanan
+//        mm.deleteMakanan(mkn);
+
+//nambah penjualan
+//        PenjualanModel pjln = new PenjualanModel();
+//        Penjualan pjl = new Penjualan(2,1,10 nama makanan);
+//        //nambahin penjualan
+//        pjl.updPenjualan(pjl);
+//        pjl.addPenjualan(pjl);
+//        //hapus penjualan
+//        pjln.deletePenjualan(pjln);         
+        
+            
+    }
+//
+//    private static class barang {
+//
+//        public barang(String string, int i, String nama_barang, int i0, int i1, int i2) {
+//        }
+//    }
+    
+}
